ce4f006b99d9b55f34b3a0c724292c18542a7f44
LMS Dev02 <lms.dev02@intechww.com>
2026-04-09 15:13:50 +0500

    ds

--- LeaveOnTaskRunner/Program.cs
+++ LeaveOnTaskRunner/Program.cs
@@ -68,7 +68,7 @@ namespace LeaveOnTaskRunner
 
             string connection = ConfigurationManager.ConnectionStrings["BioStarEntities"].ConnectionString; ;// "Data Source=10.1.10.28;Initial Catalog=BioStarTA;User Id=sa;Password=@Intech#123;";
             var userLsit=  await service.GetUserActiveList();
-           // userLsit = userLsit.Where(k => k.BioStarEmpNum == 2950).ToList();
+            userLsit = userLsit.Where(k => k.BioStarEmpNum == 2696).ToList();
 
             SqlConnection con = new SqlConnection(connection);
             con.Open();
@@ -86,7 +86,7 @@ namespace LeaveOnTaskRunner
                 InsertSyncLog("Sync User Atendance", "", userLsit.Count(),
                            1, 0, "Start Job", "Sync User Atendance", 0);
 
-            DateTime lsstartDate = new DateTime(2026, 2, 28);
+            DateTime lsstartDate = new DateTime(2026, 03, 30);
             DateTime lsendDate = new DateTime(2026, 4, 1);
 
             for (DateTime date = lsstartDate; date <= lsendDate; date = date.AddDays(1))
