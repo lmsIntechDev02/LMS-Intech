1e5753386213791729af1f6e18400ec70f2dc48d
LMS Dev02 <lms.dev02@intechww.com>
2026-04-10 12:05:50 +0500

    d

--- LeaveOnTaskRunner/Program.cs
+++ LeaveOnTaskRunner/Program.cs
@@ -40,11 +40,11 @@ namespace LeaveOnTaskRunner
             //DateTime endDate = new DateTime(2026, 02, 11);
 
             // testing
-          //  DateTime startDate = new DateTime(2026, 02, 28);
-          // DateTime endDate = new DateTime(2026, 04, 01);
+         DateTime startDate = new DateTime(2026, 04, 01);
+        DateTime endDate = new DateTime(2026, 04, 10);
 
-          DateTime startDate = yesterday;
-           DateTime endDate = today;
+       //   DateTime startDate = yesterday;
+         //  DateTime endDate = today;
 
 
 
@@ -68,7 +68,7 @@ namespace LeaveOnTaskRunner
 
             string connection = ConfigurationManager.ConnectionStrings["BioStarEntities"].ConnectionString; ;// "Data Source=10.1.10.28;Initial Catalog=BioStarTA;User Id=sa;Password=@Intech#123;";
             var userLsit=  await service.GetUserActiveList();
-           // userLsit = userLsit.Where(k => k.BioStarEmpNum == 2696).ToList();
+             userLsit = userLsit.Where(k => k.BioStarEmpNum == 2696).ToList();
 
             SqlConnection con = new SqlConnection(connection);
             con.Open();
