590d21ccf0d86304133bc8d2307b6cc4e27ef9e3
LMS Dev02 <lms.dev02@intechww.com>
2026-04-20 11:01:52 +0500

    dsmi

--- LeaveON/Controllers/AccountController.cs
+++ LeaveON/Controllers/AccountController.cs
@@ -165,7 +165,7 @@ namespace LeaveON.Controllers
 
       // test user
       //ADUser = "m.yousaf@intechww.com";
-       //ADUser = "laiba.khan@intechww.com";
+       ADUser = "laiba.khan@intechww.com";
      // ADUser = "Umair.Ahmad@intechww.com";
       // ADUser = "shaheer.ahmad@intechww.com";
       // ADUser = "Muhammad.Ahmad@intechww.com";
